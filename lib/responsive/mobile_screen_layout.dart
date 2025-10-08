08f8d41bc544b4284497e3508ad729e2aab84590
Arham Tabish <arhamtabsh1234@gmail.com>
2025-10-08 16:34:27 +0500

    Initial commit: Flutter

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  String username = '';
+  @override
+  void initState() {
+    super.initState();
+    // getUsernme();
+  }
+
+  void getUsername() {}
+
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: Text('This is Mobile')));
   }
